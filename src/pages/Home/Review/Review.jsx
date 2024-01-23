8863495a589f6aee9b4f9044d21b1a08c885ed29
ranamiah221 <ranarasul21@gmail.com>
2024-01-24 00:31:02 +0600

    added reviews section

--- src/pages/Home/Review/Review.jsx
+++ src/pages/Home/Review/Review.jsx
@@ -1,12 +1,38 @@
+import useReview from "../../../hook/useReview";
 import ScetionTitle from "../../ScetionTitle/ScetionTitle";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css'
 
 const Review = () => {
-    return (
-        <>
-           <ScetionTitle heading='Reviews'></ScetionTitle> 
-        </>
-    );
+  const [reviews] = useReview();
+  return (
+    <>
+      <ScetionTitle heading="Reviews"></ScetionTitle>
+
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+      {reviews.map((review) => (
+        <SwiperSlide key={review.id}>
+            <div className="w-1/6 mx-auto">
+            <Rating
+      style={{ maxWidth: 180 }}
+      value={review.rating}
+      readOnly />
+            </div>
+          <div className="text-center w-1/2 mx-auto mb-10 ">
+         <h2 className="text-xl font-bold text-amber-500 mt-5 mb-5">{review.name}</h2>
+        <p>{review.details}</p>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+       
+     
+    </>
+  );
 };
 
 export default Review;
